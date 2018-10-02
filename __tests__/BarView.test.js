cbf03b7b5163eb6edd4464d9c54bd092023800b5
dmmz <dmartinmartinez@gmail.com>
2018-10-02 21:03:37 +0200

    BarView tests

--- __tests__/BarView.test.js
+++ __tests__/BarView.test.js
@@ -1,11 +1,116 @@
 import BarView from '../src/BarView.js'
 
-test('BarView test', () => {
-    let barView = new BarView({
-        sub:  10,
-        sup:  10,
-        margins: {line:70} 
+describe('BarView ', () => {
+  describe('constructor', () => {
+    test('constructor with no params', () => {
+      const barView = new BarView()
+
+      expect(barView.barsPerLine).toBeDefined()
+
+      expect(barView.margins).toHaveProperty('top')
+      expect(barView.margins).toHaveProperty('left')
+      expect(barView.margins).toHaveProperty('right')
+      expect(barView.margins).toHaveProperty('line')
+      expect(barView.margins).toHaveProperty('bottom')
+
+      expect(barView.bar).toHaveProperty('padding')
+      expect(barView.bar).toHaveProperty('width')
+      expect(barView.bar).toHaveProperty('repetitionLineSpace')
+      expect(barView.bar).toHaveProperty('padding')
+
+      expect(barView.bar.padding).toHaveProperty('top')
+      expect(barView.bar.padding).toHaveProperty('left')
+
     })
-    expect(barView.barsPerLine).toBe(4)
-    expect(barView.margins.line).toBe(70)
-})
+
+    test('constructor with some values', () => {
+      const barView = new BarView({
+        margins:{
+          top: 100
+        },
+        bar:{
+          height: 200
+        }
+      })
+      expect(barView.barsPerLine).toBeDefined()
+
+      expect(barView.margins.top).toEqual(100)
+      expect(barView.margins).toHaveProperty('left')
+
+      expect(barView.bar.height).toEqual(200)
+      expect(barView.bar).toHaveProperty('padding')
+    })
+
+    test('constructor with deep property values', () => {
+      const barView = new BarView({
+        bar:{
+          padding:{
+            left:300
+          }
+        }
+      })
+      expect(barView.bar.padding.left).toEqual(300)
+      expect(barView.bar).toHaveProperty('width')
+    })
+  });
+  describe('barView methods', () => {
+    let barView
+    beforeAll(() => {
+      barView = new BarView()
+    })
+    describe('getDimensions', () => {
+
+      test('should throw exception if no arguments', () => {
+        const fn = () => { barView.getDimensions() }
+        expect(fn).toThrow()
+      })
+
+      test('should return array of 2 numbers', () => {
+        let dimensions = barView.getDimensions(0)
+        expect(dimensions).toHaveLength(2)
+        expect(dimensions[0]).toBeGreaterThanOrEqual(0)
+        expect(dimensions[1]).toBeGreaterThanOrEqual(0)
+      })
+
+    })
+    describe('getDivisor', () => {
+      test('should work for ternary time signature', () => {
+        expect(barView.getDivisor('34', 'whole')).toEqual(1)
+        expect(barView.getDivisor('34', 'half')).toEqual(2)
+        expect(barView.getDivisor('34', 'quarter')).toEqual(2)
+      })
+      test('should work for 4/4 time signature', () => {
+        expect(barView.getDivisor('44', 'whole')).toEqual(1)
+        expect(barView.getDivisor('44', 'half')).toEqual(2)
+        expect(barView.getDivisor('44', 'quarter')).toEqual(4)
+      })
+      test('undefined time signature should work like 4/4', () => {
+        expect(barView.getDivisor(undefined, 'whole')).toEqual(1)
+        expect(barView.getDivisor(undefined, 'half')).toEqual(2)
+        expect(barView.getDivisor(undefined, 'quarter')).toEqual(4)
+      })
+    });
+    describe('getStartXList', () => {
+      test('should throw exception if durations is not array', () => {
+        const fn = () => { barView.getStartXList('34','not an array') }
+        expect(fn).toThrow()
+      })
+      test('should return array of same length as durations', () => {
+        const durations = ['whole','quarter','half','quarter']
+        expect(barView.getStartXList('44', durations)).toHaveLength(durations.length)
+      })
+    });
+    describe('getHeight', () => {
+      test('should throw exception if there are no arguments', () => {
+        expect(() => { barView.getHeight()}).toThrow()
+      })
+      test('should return a number', () => {
+        expect(barView.getHeight(10)).toBeGreaterThanOrEqual(0)
+      })
+      test('should return a a number (determined by margin) if argument is 0', () => {
+        expect(barView.getHeight(0)).toBeGreaterThanOrEqual(0)
+      })
+
+    });
+  })
+});
--- src/BarView.js
+++ src/BarView.js
@@ -1,56 +1,80 @@
 class BarView {
-    
-    constructor(props) {
 
-        let width = props.width || 750
-        this.barsPerLine = props.barsPerLine || 4
-        this.margins = Object.assign({},{
-            top:    20,
-            left:   20,
-            line:   30,
-            right:  20
-        },props.margins)
+    constructor(props = {}) {
+        const DEFAULT_WIDTH = 750
+        const DEFAULT_BARS_PER_LINE = 4
+        const DEFAULT_MARGINS = {
+          top:    20,
+          left:   20,
+          line:   30,
+          right:  20,
+          bottom: 20
+        }
+        const DEFAULT_BAR_DIMENSIONS = {
+          padding: {
+            top:  25,
+            left: 8
+          },
+          height: 40,
+          repetitionLineSpace: 4
+        }
 
-        this.bar = Object.assign({},{
-            paddingTop: 25,
-            paddingLeft:8,
-            height: 40
-        }, props.bar) 
+        this.barsPerLine = props.barsPerLine || DEFAULT_BARS_PER_LINE
+        this.margins = {...DEFAULT_MARGINS, ...props.margins}
+        this.bar = {...DEFAULT_BAR_DIMENSIONS, ...props.bar}
+
+        const width = props.width || DEFAULT_WIDTH
         this.bar.width = (width - this.margins.left - this.margins.right) / this.barsPerLine
     }
 
     getDimensions(numBar) {
-        let posBar = numBar % this.barsPerLine // bar position 
-        let line = Math.floor(numBar / this.barsPerLine) // lineNumber
+      if (numBar == null) {
+        throw new TypeError('numBar argument must be a number')
+      }
+      let posBar = numBar % this.barsPerLine // bar position
+      let line = Math.floor(numBar / this.barsPerLine) // lineNumber
         let x = this.margins.left + this.bar.width * posBar
         let y = this.margins.top + (this.bar.height + this.margins.line) * line
         return [x, y]
     }
+    setDimensions(bar, numBar) {
+      const [x,y] = this.getDimensions(numBar)
+      bar.dimensions = {x,y}
+      return bar
+    }
+    getDivisor(timeSig, duration){
+      let divisor
+      if (timeSig == '34')
+        divisor = duration === 'whole' ? 1 : 2
+      else
+        divisor = duration === 'quarter'  ?  4 :
+                  duration === 'half'     ?  2 : 1
+      return divisor
+    }
     getStartXList(timeSig, durations){
-        let widthList = durations.map(duration  => {
-            let divisor
-            if (timeSig == '34')
-                divisor = duration === 'whole' ? 1 : 2
-            else
-                divisor = duration === 'quarter'  ?  4 :
-                          duration === 'half'     ?  2 : 1  
+      if (!Array.isArray(durations)){
+        throw new TypeError('durations should be an array')
+      }
 
-            return (this.bar.width - this.bar.padding.left) / divisor
-        })
-        var currentX = this.bar.padding.left;
-        let startXList = [currentX];
-        for (let i = 0; i < widthList.length - 1; i++) {//don't care about last width, getting starting points
-            currentX += widthList[i];
-            startXList.push(currentX)
-        }
-        return startXList;
+      let widthList = durations.map(duration =>
+        (this.bar.width - this.bar.padding.left) / this.getDivisor(timeSig, duration)
+      )
+      let currentX = this.bar.padding.left;
+      let startXList = [currentX];
+
+      // until length - 1 because don't care about last width, getting starting points
+      for (let i = 0; i < widthList.length - 1; i++) {
+        currentX += widthList[i];
+        startXList.push(currentX)
+      }
+      return startXList;
     }
-    getWritableBarWidth() {
-        return this.bar.width - this.bar.padding.left
-    }
+
     getHeight(totalBars){
-        const marginBottom = 20
-        return Math.ceil(totalBars / this.barsPerLine) * (this.bar.height + this.margins.line) + marginBottom
+      if (totalBars == null) {
+        throw new TypeError('totalBars should be a number')
+      }
+      return Math.ceil(totalBars / this.barsPerLine) * (this.bar.height + this.margins.line) + this.margins.bottom
     }
 }
 export default BarView;
