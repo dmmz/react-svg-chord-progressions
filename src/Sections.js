ff509812697ab3fc8edd28543256a8d426ed3fb9
dmmz <dmartinmartinez@gmail.com>
2019-08-02 18:47:18 +0200

    all files formatted with prettier

--- src/Sections.js
+++ src/Sections.js
@@ -1,52 +1,51 @@
 const Sections = (bars, settings = {}) => {
-    const DEFAULT_TEXT_COLOR = '#333'
-    const DEFAULT_TEXT_SIZE = 13
+  const DEFAULT_TEXT_COLOR = "#333";
+  const DEFAULT_TEXT_SIZE = 13;
 
-    const textColor = settings.textColor || DEFAULT_TEXT_COLOR
-    const textSize = settings.textSize || DEFAULT_TEXT_SIZE
+  const textColor = settings.textColor || DEFAULT_TEXT_COLOR;
+  const textSize = settings.textSize || DEFAULT_TEXT_SIZE;
 
-    const getX = bar => bar.dimensions.x + 15
-    const getY = bar => bar.dimensions.y
-    const getDefaultSettings = () => ({textColor, textSize})
+  const getX = bar => bar.dimensions.x + 15;
+  const getY = bar => bar.dimensions.y;
+  const getDefaultSettings = () => ({ textColor, textSize });
 
-    let sectionBars = bars.filter(bar => !!bar.section)
+  let sectionBars = bars.filter(bar => !!bar.section);
 
-    let hRect = 15 //rectangle's height
+  let hRect = 15; //rectangle's height
 
-    let sectionTexts = []
-    let sectionRectangles = []
-    let yRect, yText, wRect
+  let sectionTexts = [];
+  let sectionRectangles = [];
+  let yRect, yText, wRect;
 
-    sectionBars.forEach((bar) => {
+  sectionBars.forEach(bar => {
+    yRect = getY(bar) - hRect - 5;
+    yText = yRect + 0.8 * hRect;
+    wRect = 10 + 5 * bar.section.length; // rectangle's width
 
-        yRect = getY(bar) - hRect - 5
-        yText = yRect + 0.8 * hRect
-        wRect = 10 + 5 * bar.section.length // rectangle's width
+    sectionTexts.push({
+      x: getX(bar) - wRect / 2,
+      y: yText,
+      text: bar.section,
+      fill: textColor,
+      fontSize: textSize
+    });
 
-        sectionTexts.push({
-            x: getX(bar) - wRect / 2,
-            y: yText,
-            text: bar.section,
-            fill: textColor, 
-            fontSize: textSize
-        })
-
-        sectionRectangles.push({
-            x: getX(bar) - wRect / 2,
-            y: yRect,
-            width: wRect,
-            height: hRect,
-            fill: 'none',
-            stroke: 'black',
-            strokeWidth: 1,
-        })
+    sectionRectangles.push({
+      x: getX(bar) - wRect / 2,
+      y: yRect,
+      width: wRect,
+      height: hRect,
+      fill: "none",
+      stroke: "black",
+      strokeWidth: 1
+    });
+  });
+  return {
+    getDefaultSettings,
+    getSections: () => ({
+      texts: sectionTexts,
+      rects: sectionRectangles
     })
-    return {
-        getDefaultSettings,
-        getSections: () => ({
-          texts: sectionTexts,
-          rects: sectionRectangles
-        })
-    }
-}
-export default Sections
+  };
+};
+export default Sections;
--- src/getTimeSignature.js
+++ src/getTimeSignature.js
@@ -1,33 +1,36 @@
 const getTimeSignature = (bars, barView, settings = {}) => {
-  if (!bars[0].timeSignature){
-    throw new TypeError('first bar has no time signature')
+  if (!bars[0].timeSignature) {
+    throw new TypeError("first bar has no time signature");
   }
 
-  const DEFAULT_SIZE = 20
-  const DEFAULT_COLOR = '#000'
-  const color = settings.color || DEFAULT_COLOR
-  const size = settings.size || DEFAULT_SIZE
+  const DEFAULT_SIZE = 20;
+  const DEFAULT_COLOR = "#000";
+  const color = settings.color || DEFAULT_COLOR;
+  const size = settings.size || DEFAULT_SIZE;
 
-  let timeSig = bars[0].timeSignature
+  let timeSig = bars[0].timeSignature;
   let attrTimeSig = {
     fill: color,
     fontSize: size,
-    textAnchor: 'start'
-  }
-  let timeSigX = bars[0].dimensions.x -15
-  let baseY = bars[0].dimensions.y + barView.bar.height / 2 + 5
-  let distance = 10
+    textAnchor: "start"
+  };
+  let timeSigX = bars[0].dimensions.x - 15;
+  let baseY = bars[0].dimensions.y + barView.bar.height / 2 + 5;
+  let distance = 10;
 
-  return [{
-      x:  timeSigX,
-      y:  baseY - distance ,
+  return [
+    {
+      x: timeSigX,
+      y: baseY - distance,
       text: timeSig[0].toString(),
       ...attrTimeSig
-    },{
-      x: timeSigX ,
+    },
+    {
+      x: timeSigX,
       y: baseY + distance,
       text: timeSig[1].toString(),
       ...attrTimeSig
-   }]
-}
-export default getTimeSignature
+    }
+  ];
+};
+export default getTimeSignature;
