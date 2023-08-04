4d950c8cb3e3955b178fc45ef3481c96d0ad6260
dmmz <dmartinmartinez@gmail.com>
2023-08-04 20:24:03 +0200

    feat: improved example

--- example/src/index.js
+++ example/src/index.js
@@ -1,24 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { render } from "react-dom";
+import { useState, useRef } from "react";
 import ChordProgression from "../../src/ChordProgression";
 import aloneTogether from "./alone-together.js";
 import "./style.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>This is a ChordProgression example</p>
-        <ChordProgression
-          bars={aloneTogether}
-          colorActive={"#00f"}
-          activeBar={2}
-          colorSelected={"#aa0"}
-          selectedBars={[1, 4]}
-          barMouseDown={() => console.log("mouse down")}
-          barMouseOver={() => console.log("mouse over")}
-          barMouseUp={() => console.log("mouse up")}
-          selections={[
+const usePlayer = (maxBars) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const playerRef = useRef(null);
+  const [barNum, setBarNum] = useState(null);
+
+  const toggleIsPlaying = () => setIsPlaying((isPlaying) => !isPlaying);
+  const stop = () => {
+    setIsPlaying(false);
+    setBarNum(null);
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      playerRef.current = setInterval(() => {
+        setBarNum((barNum) =>
+          barNum === null || barNum === maxBars ? 0 : ++barNum
+        );
+      }, 250);
+    } else {
+      clearInterval(playerRef.current);
+    }
+    return () => clearInterval(playerRef.current);
+  }, [isPlaying]);
+  return { barNum, isPlaying, toggleIsPlaying, stop };
+};
+
+const foldedAloneTogetherBars = 32;
+
+const Example = () => {
+  const [showSelections, setShowSelections] = useState(false);
+  const { barNum, isPlaying, toggleIsPlaying, stop } = usePlayer(
+    foldedAloneTogetherBars
+  );
+
+  const toggleShowSelections = () =>
+    setShowSelections((showSelections) => !showSelections);
+
+  return (
+    <div>
+      <div id="header">
+        <h1>ChordProgression example</h1>
+        <p>Score scrolls following cursor if score overflows viewport.</p>
+        <button onClick={toggleIsPlaying}>
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+        <button onClick={stop}>Stop</button>
+        <button onClick={toggleShowSelections}>
+          {showSelections ? "Hide selections" : "Show selections"}
+        </button>
+      </div>
+      <ChordProgression
+        bars={aloneTogether}
+        colorActive={"#00f"}
+        activeBar={barNum}
+        colorSelected={"#aa0"}
+        selectedBars={showSelections && [1, 4]}
+        barMouseDown={() => console.log("mouse down")}
+        barMouseOver={() => console.log("mouse over")}
+        barMouseUp={() => console.log("mouse up")}
+        selections={
+          showSelections && [
             {
               start: { chord: 0, bar: 10 },
               end: { chord: 1, bar: 11 },
@@ -29,10 +76,16 @@ class App extends React.Component {
               end: { chord: 0, bar: 18 },
               color: "#060",
             },
-          ]}
-        />
-      </div>
-    );
+          ]
+        }
+      />
+    </div>
+  );
+};
+
+class App extends React.Component {
+  render() {
+    return <Example />;
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: path.join(__dirname, "example/src/index.html"),
-  filename: "./index.html"
+  filename: "./index.html",
 });
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: "./example/src/index.js",
   plugins: [htmlWebpackPlugin],
   module: {
@@ -14,26 +14,29 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         use: "babel-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|woff|woff2|eot|ttf)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'assets/[hash][ext][query]',
-        }
-      }
-    ]
+          filename: "assets/[hash][ext][query]",
+        },
+      },
+    ],
+  },
+  performance: {
+    hints: false,
   },
   devServer: {
-    port: 3001
+    port: 3001,
   },
   output: {
     clean: true, // Clean the output directory before each build
-    assetModuleFilename: 'assets/[hash][ext][query]',
-  }
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
 };
